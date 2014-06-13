7c3fd8e2680fbecec55242271cf7ec05707860ae
Anirudh <asridhar@salesforce.com>
2014-06-13 15:54:57 -0700

    Variables

---
+++ flow-control.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Simple If examples
+ANSWER=true
+if [[ "$ANSWER" = true ]]; then
+	echo "Entered If Loop";
+fi
---
+++ variables.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# -----------------
+# VARIABLE NAMING
+# -----------------
+# Underscore separated (time_to_live)
+# All uppercase for Environment/Global variables (TOOL_VERSION)
+# All lowercase for local variables (error_message)
+# Case sensitive
+
+# Valid names [0-9a-zA-Z_]
+# TTL_2, _TTL, T_2_T
+
+# Invalid variable names
+# 2_Name, Name!
+
+# -----------
+# ASSIGNMENT
+# -----------
+string_var="Hello World" 			# Strings
+num_var=500							# Numbers
+boolean_var=true					# Boolean
+another_string_var=string_var		# Can assign one variable to another 
+another_num_var=num_var+500			# Can use one variable in another
+user_name=`whoami`; echo $user_name	# Can assign result of executed commands
+
+# Invalid (no spaces)
+# VAR = "value"
+
+# Read only variables
+READ_ONLY="Message"
+readonly READ_ONLY
+
+# Unsetting variables
+message="Hello World"
+echo $message
+unset message 						# No qualifying $
+echo $message
+
+# -----------------
+# SPECIAL VARIABLES
+# -----------------
+# $0 -> Filename of current script
+# $# -> Number of command-line arguments
+# $1, $2... -> 1st, 2nd etc command-line argument
+# $@ -> Quoted arguments, $1 $2
+# $? -> Exit code of last command
+list=`ls -la | wc -l`; echo $?
+list=`unknown`; echo $?
+
